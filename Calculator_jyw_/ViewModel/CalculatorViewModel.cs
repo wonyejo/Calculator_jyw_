aec738d6872e5ffe69064194f6323e9639bb1619
wonyejo <141125658+wonyejo@users.noreply.github.com>
2023-08-14 16:09:33 +0900

    부동소수점 오차 해결해려고 시도해봄

--- Calculator_jyw_/ViewModel/CalculatorViewModel.cs
+++ Calculator_jyw_/ViewModel/CalculatorViewModel.cs
@@ -13,15 +13,15 @@ namespace Calculator_jyw_
     public class CalculatorViewModel : INotifyPropertyChanged
     {
 
+        public const Double Epsilon = 4.94065645841247E-324;
+
+
         private string inputText = "";
         private string resultText = "";
         private string Operator;
         private bool doublePointEntered = false;
-        Stack<string> operatorStack = new Stack<string>();
-        List<string> outputList = new List<string>();
-
-       
-
+        private Stack<string> operatorStack = new Stack<string>();
+        private List<string> outputList = new List<string>();
         public string InputText
         {
             get { return inputText; }
@@ -38,6 +38,15 @@ namespace Calculator_jyw_
             {
                 resultText = value;
                 OnPropertyChanged("resultText");
+            }
+        }
+        public List<string> OutputList
+        {
+            get { return outputList; }
+            set
+            {
+                outputList = value;
+                OnPropertyChanged("outputList");
             }
         }
 
@@ -58,15 +67,6 @@ namespace Calculator_jyw_
         }
 
 
-        /*
-        * @brief 숫자 버튼을 누르면 InputTextBox에 해당 숫자가 입력됩니다.  
-        * @param parameter: 누른 숫자의 값
-        * @return 반환값 없음
-        * @note Patch-notes
-        * 2023-08-10|조예원|설명작성
-        * @warning 없음
-        */
-
         private string ConvertToPostfix(string infixExpression)
         {
             Dictionary<string, int> precedence = new Dictionary<string, int>
@@ -78,7 +78,7 @@ namespace Calculator_jyw_
 
             foreach (string token in tokens)
             {
-                if (int.TryParse(token, out int _))
+                if (double.TryParse(token, out double _))
                 {
                     outputList.Add(token);
                 }
@@ -103,6 +103,7 @@ namespace Calculator_jyw_
                     }
                     operatorStack.Push(token);
                 }
+               
             }
 
             while (operatorStack.Count > 0)
@@ -113,23 +114,53 @@ namespace Calculator_jyw_
             return string.Join(" ", outputList);
         
     }
-        private int CalculatePostfix(string postfixExpression)
+
+        //private double AdjustValue(double value, double epsilon)
+        //{
+        //    double roundedValue = Math.Round(value, 2); // 예시로 소수점 아래 2자리에서 반올림
+        //    double ceilingValue = Math.Ceiling(value * 100) / 100; // 소수점 아래 2자리에서 올림
+        //    double floorValue = Math.Floor(value * 100) / 100; // 소수점 아래 2자리에서 내림
+
+        //    double diffFromRounded = Math.Abs(value - roundedValue);
+        //    double diffFromCeiling = Math.Abs(value - ceilingValue);
+        //    double diffFromFloor = Math.Abs(value - floorValue);
+
+        //    double minDiff = Math.Min(diffFromRounded, Math.Min(diffFromCeiling, diffFromFloor)); //이렇게 작은 수는 동일하게 취급하는데 어떻게 이걸로 부동 소수점 오차를 보정하나요?
+
+        //    if (minDiff < epsilon)
+        //    {
+        //        if (minDiff == diffFromRounded)
+        //            return roundedValue;
+        //        else if (minDiff == diffFromCeiling)
+        //            return ceilingValue;
+        //        else
+        //            return floorValue;
+        //    }
+        //    else
+        //    {
+        //        return value; // 그대로 반환
+        //    }
+        //}
+        private double CalculatePostfix(string postfixExpression)
         {
-            Stack<int> stack = new Stack<int>();
+            Stack<double> stack = new Stack<double>();
 
             string[] tokens = postfixExpression.Split(' ');
 
             foreach (string token in tokens)
             {
-                if (int.TryParse(token, out int number))
+                if (double.TryParse(token, out double number))
                 {
                     stack.Push(number);
                 }
                 else
                 {
-                    int operand2 = stack.Pop();
-                    int operand1 = stack.Pop();
-                    int result = PerformOperation(operand1, operand2, token);
+                    double operand2 = stack.Pop();
+                    double operand1 = stack.Pop();
+                    double result = PerformOperation(operand1, operand2, token);
+
+                    //AdjustValue(result, Epsilon);
+
                     stack.Push(result);
                 }
             }
@@ -138,7 +169,7 @@ namespace Calculator_jyw_
         }
 
   
-        private int PerformOperation(int operand1, int operand2, string operation)
+        private double PerformOperation(double operand1, double operand2, string operation)
         {
             switch (operation)
             {
@@ -150,7 +181,14 @@ namespace Calculator_jyw_
                 default: throw new ArgumentException("Invalid operation: " + operation);
             }
         }
-
+        /*
+        * @brief 숫자 버튼을 누르면 InputTextBox에 해당 숫자가 입력됩니다.  
+        * @param parameter: 누른 숫자의 값
+        * @return 반환값 없음
+        * @note Patch-notes
+        * 2023-08-10|조예원|설명작성
+        * @warning 없음
+        */
         private void NumberButtonCommandExecute(object parameter)
         {
 
@@ -184,8 +222,18 @@ namespace Calculator_jyw_
         public void OperatorButtonCommandExecute(object parameter)
         {
             doublePointEntered = false;
+
+            if (!string.IsNullOrEmpty(Operator)) // 이전에 연산자가 입력되었을 경우
+            {
+                // 이전 연산자를 지우고 새로운 연산자 입력
+                inputText = inputText.Remove(inputText.Length - 2);
+            
+            }
+
+
             Operator = parameter.ToString();
-            InputText = $"{inputText}{" "}{parameter}{" "}";
+            inputText=$"{inputText}{" "}{parameter}{" "}";
+            InputText = inputText;
 
         }
         /*
@@ -210,14 +258,14 @@ namespace Calculator_jyw_
             InputText = "";
             ResultText = "";
             Operator = null;
+            doublePointEntered = false;
          
         }
 
         private void ResultButtonCommandExecute(object parameter)
-        {   string tmp = inputText;
-            InputText = ResultText;
+        {   string tmp = InputText;
+            ResultText = $"{InputText}{ " = " }";
             InputText=CalculatePostfix(ConvertToPostfix(tmp)).ToString();
-
 
         }
         /*
